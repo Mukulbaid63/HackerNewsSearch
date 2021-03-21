87810cc576109c0cc2b98a216732805c1818100b
Muddhit Baid <61585314+Mukulbaid63@users.noreply.github.com>
2021-03-21 16:41:36 +0545

    Update QueryOutput.js

--- src/QueryOutput.js
+++ src/QueryOutput.js
@@ -1,35 +1,64 @@
-import React from 'react';
-import './QueryOutput.css'
-const QueryOutput = ({result,query}) => {
-    const emptyHandler=()=>{
-        if(result.length==0 && query!=""){
-            return (
-                <p style={{display:'flex',justifyContent:'center',alignContent:'center',fontFamily:'Lato',fontSize:'30px',fontWeight:'700'}}>We found no stories matching {query}</p>
-            )
-        }
+import React, { useEffect } from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import TuneIcon from '@material-ui/icons/Tune';
+const QueryInput = ({query,setquery,fetchQuery,setSelectState,selectState}) => {
+
+
+    function handleStateChange(e) {
+		setSelectState(e.target.value);
+	}
+    const changeHandler=(evt)=>{
+        setquery(evt.target.value);
     }
+    useEffect(() => {
+        fetchQuery();
+       
+    }, [query])
     return (
-        <div style={{backgroundColor:'#f7f7f7'}}>
-            {emptyHandler()}
-            {
-                result.map((item,itemindex)=>(
-                    <>
-                   <div style={{display:'flex',fontFamily:'Lato',fontSize:'17px',marginLeft:'15px'}}>
-                    <div key={itemindex}>{item.title}
-                    
-                   {
-                       item.url!=null&&<a href={item.url} style={{fontSize:'15px',color:'#878380',textDecoration:'none',marginLeft:'5px'}}>({item.url})</a>
-                   }
-                    </div>
-                    </div> 
-                    <div style={{display:'flex',fontSize:'12px',color:'#878380',marginLeft:'15px',marginTop:'5px'}}>
-                    <span style={{padding:'0px 3px',margin:'0px 3px'}}>{item.points} points</span>|<span style={{padding:'0px 3px',margin:'0px 3px'}}>{item.author}</span>|<span style={{padding:'0px 3px',margin:'0px 3px'}}>{item.num_comments} comments</span>
-                    </div>
-                    </>
-                ))
-            }
+        <>
+        <div style={{display:"flex",fontFamily:'Lato',alignItems: "center",backgroundColor:'#e86813',border:'2px solid #e86813'}}>   
+                <p style={{color:'white',border:'1px solid white',fontSize:'22px',padding:'4px 10px',margin:'0px 20px'}}>H</p>
+                <SearchIcon style={{color:'#e86813',backgroundColor:'white',fontSize:'36px'}}/>
+
+                    <input style={{outline:"none",border:'none',padding:'0px',height:'36px',width:'80vw',margin:'0px'}} placeholder="Search stories by title, url or author" onChange={changeHandler}>
+
+
+                    </input>
+                    &nbsp;&nbsp;by&nbsp;&nbsp;
+                    <span style={{color:'white',margin:'0px 10px'}}> Algolia</span>
+                    <TuneIcon style={{color:'white',margin:'0px 10px'}}/>
+
         </div>
+        <div style={{backgroundColor:'#f7f7f7',padding:'15px'}}>
+            Search
+        <select onChange={handleStateChange} style={{marginLeft:'5px',fontSize:'15px'}}>
+            <option value="" >All </option>
+            <option value="story" selected> Stories</option>
+            <option value="job"> Job</option>
+            <option value="poll"> Poll</option>
+            <option value="pollopt" > Pollopt</option>
+            <option value="comment"> Comments</option>
+
+        </select>
+        &nbsp;by 
+        <select style={{marginLeft:'5px',fontSize:'15px'}}>
+            
+            <option selected> Popularity</option>
+            <option> Date</option>
+
+        </select>
+        &nbsp;for
+        <select style={{marginLeft:'5px',fontSize:'15px'}}>
+            <option selected>All time</option>
+            <option>Last 24h</option>
+            <option>Past Week</option>
+            <option>Past Month</option>
+            <option>Past Year</option>
+            <option>Custom Range</option>
+        </select>
+        </div>
+        </>
     );
 };
 
-export default QueryOutput;
+export default QueryInput;
